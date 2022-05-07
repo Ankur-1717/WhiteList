b01a747f0d021746b1b7ce2fba6cc7ca1c613644
Ankur-1717 <ankur.ambar17@gmail.com>
2022-05-07 15:04:47 +0530

    removed files

--- ethereum/scripts/deploy.js
+++ ethereum/scripts/deploy.js
@@ -1,10 +1,6 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  /*
-  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so whitelistContract here is a factory for instances of our Whitelist contract.
-  */
   const whitelistContract = await ethers.getContractFactory("Whitelist");
 
   // here we deploy the contract
